54165f0a2fbfee090b30b01b111a575d214dccaf
varungujarathi9 <v.gujarathi777@gmail.com>
2024-08-17 19:58:29 -0700

    feat: add git plugins

---
+++ lua/plugins/git-plugins.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function ()
+      require('gitsigns').setup({
+        current_line_blame = true,
+      })
+      vim.keymap.set("n", "<leader>gp", ":Gitsign preview_hunk<CR>", {})
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+  }
+}
