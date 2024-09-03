47eb1b00b588635fedc1da084319754e77b1ca6b
varungujarathi9 <v.gujarathi777@gmail.com>
2024-09-02 22:34:39 -0700

    chore: retab one dark file

--- lua/plugins/one-dark.lua
+++ lua/plugins/one-dark.lua
@@ -1,11 +1,11 @@
 return {
-	"navarasu/onedark.nvim",
-	priority = 1000,
-	lazy = false,
-	config = function()
+  "navarasu/onedark.nvim",
+  priority = 1000,
+  lazy = false,
+  config = function()
     require('onedark').setup({
       style = 'cool'
     })
     require('onedark').load()
-	end,
+  end,
 }
