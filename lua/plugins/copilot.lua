9dc8f3322c8abe3d58bd7e1fda3bf07a6b44aa82
Varun Gujarathi <v.gujarathi777@gmail.com>
2025-04-30 07:06:25 -0400

    update copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,16 +2,4 @@ return{
   {
     "github/copilot.vim"
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      debug = true, -- Enable debugging
-    },
-  },
 }
